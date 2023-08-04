c7b64af48e2d9bf6424f6b010289e06adfeeea81
ablancaa <ablancaa@gmail.com>
2023-08-04 18:24:44 +0200

    init

--- src/main.js
+++ src/main.js
@@ -1,6 +1,8 @@
+import "bootstrap/dist/css/bootstrap.min.css"
 import { createApp } from 'vue'
 import App from './App.vue'
 import './registerServiceWorker'
 import router from './router'
 
 createApp(App).use(router).mount('#app')
+import "bootstrap/dist/js/bootstrap.js"
--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,5 +1,49 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
+  <div class="container">
+    <h1>Bítacora de trabajo</h1>
+
+    
+
   </div>
+  <div class="container">
+  <div class="row">
+    <div class="col-xs col-md"><ul class="list-group">
+      <li class="list-group-item">Instalación de Bootstrap 5</li>
+      <li class="list-group-item">Maquetación de la web/app</li>
+    </ul></div>
+    <div class="col-xs col-md"><ul class="list-group">
+      <li class="list-group-item">Instalación de Bootstrap 5</li>
+    </ul></div>
+    <div class="col-xs col-md"><ul class="list-group">
+      <li class="list-group-item">Instalación de Bootstrap 5</li>
+    </ul></div>
+  </div>
+  <hr/>
+  <div class="row">
+    <div class="col-xs col-md">
+      <div class="bg-primary text-white">.bg-primary</div>
+    <ul class="list-group">
+      <li class="list-group-item">Instalación de Bootstrap 5</li>
+    </ul></div>
+    <div class="col-xs col-md">
+      <div class="bg-primary text-white">.bg-primary</div>
+    <ul class="list-group">
+      <li class="list-group-item">Instalación de Bootstrap 5</li>
+    </ul></div>
+  </div>
+</div>
+<br/>
+  <!-- <button type="button" class="btn btn-primary">Primary</button>
+    <button type="button" class="btn btn-secondary">Secondary</button>
+    <button type="button" class="btn btn-success">Success</button>
+    <button type="button" class="btn btn-danger">Danger</button>
+    <button type="button" class="btn btn-warning">Warning</button>
+    <button type="button" class="btn btn-info">Info</button>
+    <button type="button" class="btn btn-light">Light</button>
+    <button type="button" class="btn btn-dark">Dark</button>
+    <button type="button" class="btn btn-link">Link</button> -->
 </template>
+<script setup>
+
+
+</script>
