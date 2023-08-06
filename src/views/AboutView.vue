4241b1bd364c377b7416fcca0022736f62f00680
ablancaa <ablancaa@gmail.com>
2023-08-07 00:17:15 +0200

    Componente nuevo

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -23,15 +23,63 @@
     <div class="col-xs col-md">
       <div class="bg-primary text-white">.bg-primary</div>
     <ul class="list-group">
-      <li class="list-group-item">Instalación de Bootstrap 5</li>
-    </ul></div>
+      <li class="list-group-item"><CreditCard_1/></li>
+    </ul>
+    <ul class="list-group">
+      <li class="list-group-item"><CreditCard_2/></li>
+    </ul>
+    <ul class="list-group">
+      <li class="list-group-item"><Card_2/></li>
+    </ul>
+    <ul class="list-group">
+      <li class="list-group-item"><Card_4/></li>
+    </ul>
+    <ul class="list-group">
+      <li class="list-group-item"><Switch_2/><Button_2/></li>
+    </ul>
+    <ul class="list-group">
+      <li class="list-group-item"><Loader_2/></li>
+    </ul>
+    <ul class="list-group">
+      <li class="list-group-item card_1"><Loader_3/></li>
+    </ul>
+    <ul class="list-group">
+      <li class="list-group-item card_1"><Input_1/></li>
+    </ul>
+    <ul class="list-group">
+      <li class="list-group-item"><Button_1/></li>
+    </ul>
+    <ul class="list-group">
+      <li class="list-group-item"></li>
+    </ul>
+    </div>
     <div class="col-xs col-md">
       <div class="bg-primary text-white">.bg-primary</div>
     <ul class="list-group">
-      <li class="list-group-item">Instalación de Bootstrap 5</li>
-    </ul></div>
+      <li class="list-group-item"><CreditCard_3/></li>
+    </ul>
+    <ul class="list-group">
+      <li class="list-group-item"><CreditCard_4/></li>
+    </ul>
+    <ul class="list-group">
+      <li class="list-group-item card_1"><Card_1/></li>
+    </ul>
+    <ul class="list-group">
+      <li class="list-group-item card_2"><Switch_1/></li>
+    </ul>
+    <ul class="list-group">
+      <li class="list-group-item card_2"><Loader_1/></li>
+    </ul>
+    <ul class="list-group">
+      <li class="list-group-item"><Loader_4/></li>
+    </ul>
+    <ul class="list-group">
+      <li class="list-group-item"><Loader_5/></li>
+    </ul>
+    </div>
   </div>
 </div>
+
 <br/>
   <!-- <button type="button" class="btn btn-primary">Primary</button>
     <button type="button" class="btn btn-secondary">Secondary</button>
@@ -44,6 +92,30 @@
     <button type="button" class="btn btn-link">Link</button> -->
 </template>
 <script setup>
-
+import CreditCard_1 from '@/components/CreditCard_1';
+import CreditCard_2 from '@/components/CreditCard_2';
+import CreditCard_3 from '@/components/CreditCard_3';
+import CreditCard_4 from '@/components/CreditCard_4';
+import Card_1 from '@/components/Card_1';
+import Card_2 from '@/components/Card_2';
+import Card_4 from '@/components/Card_4';
+import Switch_1 from '@/components/Toggle_switches_1.vue';
+import Switch_2 from '@/components/Toggle_switches_2.vue';
+import Loader_1 from '@/components/Loader_1.vue';
+import Loader_2 from '@/components/Loader_2.vue';
+import Loader_3 from '@/components/Loader_3.vue';
+import Loader_4 from '@/components/Loader_4.vue';
+import Loader_5 from '@/components/Loader_5.vue';
+import Input_1 from '@/components/Input_1.vue';
+import Button_1 from '@/components/Button_1';
+import Button_2 from '@/components/Button_2';
 
 </script>
+<style  scoped>
+.card_1{
+  width: 450px;
+}
+.card_2{
+  background-color: black;
+}
+</style>
