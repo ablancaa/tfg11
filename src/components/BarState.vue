8c85b4042efbb2469a1566aa420e53b280cb5558
ablancaa <ablancaa@gmail.com>
2023-08-22 17:29:01 +0200

    Cambio css

--- src/components/BarState.vue
+++ src/components/BarState.vue
@@ -19,13 +19,15 @@ const props = defineProps({
     flex-direction: row;
     flex-wrap: wrap;
     justify-content: center;
-    align-items: normal;
+    align-items: center;
     align-content: space-around;
     margin: 2px;
-    background-color: rgb(174, 185, 250);
-    height: 50px;
+    background-color:rgb(207, 223, 252);
+    height: 40px;
+    box-shadow: 0 3px 6px rgba(0, 21, 255, 0.345), 0 3px 6px rgba(0, 0, 0, 0.23);
+    margin-bottom: 10px;
 }
 .item{
-    
+    margin-top: 6px;
 }
 </style>
